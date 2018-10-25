143c7fa189f1e0c155fa4ff2cde522d71d75d0fa
loulou123546 <loulou.valleres@live.fr>
2018-10-25 14:55:22 +0200

    Add first texture, script and code basics

---
+++ On The CALL/Assets/UI/menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menu : MonoBehaviour {
+
+	public GameObject[] toDisable;
+	public GameObject loadtext;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeScene (int nb) {
+		for (int i = 0; i < toDisable.Length; i++){
+			toDisable [i].GetComponent<Button> ().interactable = false;
+		}
+		loadtext.GetComponent<Text> ().text = "Loading ...";
+		SceneManager.LoadSceneAsync (nb);
+	}
+}
