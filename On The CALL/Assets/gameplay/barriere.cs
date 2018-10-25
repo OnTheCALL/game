a68993e9a819a3cb6558f3aecbc72a2c1c4263d0
loulou123546 <loulou.valleres@live.fr>
2018-10-26 00:24:36 +0200

    Ajout des barrières

---
+++ On The CALL/Assets/gameplay/barriere.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barriere : MonoBehaviour {
+
+	Hashtable opening = new Hashtable();
+	Hashtable closing = new Hashtable();
+	public GameObject bar_L;
+	public GameObject bar_R;
+	Vector3 xPosL;
+	Vector3 xPosR;
+
+	// Use this for initialization
+	void Start () {
+		//opening.Add ("name", "Opening");
+		opening.Add ("x", 0.05f);
+		opening.Add ("time", 2.0f);
+		//closing.Add ("name", "Closing");
+		closing.Add ("x", 1.0f);
+		//closing.Add ("delay", 0.5f);
+		closing.Add ("time", 3.0f);
+		xPosL = bar_L.GetComponent<Transform> ().position;
+		xPosR = bar_R.GetComponent<Transform> ().position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator Wait(float nb){
+		yield return new WaitForSeconds (nb);
+	}
+
+	void OnTriggerEnter(Collider col){
+		iTween.Stop (bar_L);
+		iTween.Stop (bar_R);
+		StartCoroutine(Wait(0.1f));
+		iTween.ScaleTo (bar_L, opening);
+		iTween.MoveTo (bar_L, new Vector3 (xPosL.x, xPosL.y - 0.9f, xPosL.z), 2.0f);
+		iTween.ScaleTo (bar_R, opening);
+		iTween.MoveTo (bar_R, new Vector3 (xPosR.x, xPosR.y + 0.9f, xPosR.z), 2.0f);
+	}
+
+	void OnTriggerExit(Collider col){
+		iTween.ScaleTo (bar_L, closing);
+		iTween.MoveTo (bar_L, xPosL, 3.0f);
+		iTween.ScaleTo (bar_R, closing);
+		iTween.MoveTo (bar_R, xPosR, 3.0f);
+	}
+}
---
+++ On The CALL/Assets/gameplay/perso.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class perso : MonoBehaviour {
+
+	public float court = 2.7f;
+	public float marche = 1.1f;
+	public GameObject myself;
+	public GameObject skin;
+	float rotateSkin = 0.0f;
+	private float moveTx = 0.0f;
+	private float moveTy = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveTx = 0.0f;
+		moveTy = 0.0f;
+		if(Input.GetKey(KeyCode.Z)){
+			moveTy = Time.deltaTime;
+		} else if(Input.GetKey (KeyCode.S)){
+			moveTy = -Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.D)){
+			moveTx = Time.deltaTime;
+		} else if(Input.GetKey (KeyCode.Q)){
+			moveTx = -Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) {
+			myself.GetComponent<Transform> ().Translate (marche * moveTx, marche * moveTy, 0.0f);
+		} else {
+			myself.GetComponent<Transform> ().Translate (court * moveTx, court * moveTy, 0.0f);
+		}
+
+		if (moveTx > 0 && moveTy == 0) {
+			skin.GetComponent<Transform> ().localRotation = Quaternion.Euler (0.0f, 0.0f, -90.0f);
+		} else if (moveTx < 0 && moveTy == 0) {
+			skin.GetComponent<Transform> ().localRotation = Quaternion.Euler (0.0f, 0.0f, 90.0f);
+		} else if (moveTx == 0 && moveTy > 0) {
+			skin.GetComponent<Transform> ().localRotation = Quaternion.Euler (0.0f, 0.0f, 0.0f);
+		} else if (moveTx == 0 && moveTy < 0) {
+			skin.GetComponent<Transform> ().localRotation = Quaternion.Euler (0.0f, 0.0f, 180.0f);
+		} else if (moveTx > 0 && moveTy > 0) {
+			skin.GetComponent<Transform> ().localRotation = Quaternion.Euler (0.0f, 0.0f, -45.0f);
+		} else if (moveTx > 0 && moveTy < 0) {
+			skin.GetComponent<Transform> ().localRotation = Quaternion.Euler (0.0f, 0.0f, -135.0f);
+		} else if (moveTx < 0 && moveTy > 0) {
+			skin.GetComponent<Transform> ().localRotation = Quaternion.Euler (0.0f, 0.0f, 45.0f);
+		} else if (moveTx < 0 && moveTy < 0) {
+			skin.GetComponent<Transform> ().localRotation = Quaternion.Euler (0.0f, 0.0f, 135.0f);
+		}
+			
+
+		/*
+		rotateSkin = 0.0f;
+		if (Input.GetKey (KeyCode.Z)) {
+			if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) {
+				myself.GetComponent<Transform> ().Translate (0.0f, marche * Time.deltaTime, 0.0f);
+			} else {
+				myself.GetComponent<Transform> ().Translate (0.0f, court * Time.deltaTime, 0.0f);
+			}
+		} else if(Input.GetKey (KeyCode.S)) {
+			rotateSkin = 180.0f;
+			skin.GetComponent<Transform> ().localRotation = Quaternion.Euler (0.0f, 0.0f, 180.0f);
+			if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) {
+				myself.GetComponent<Transform> ().Translate (0.0f, -marche * Time.deltaTime, 0.0f);
+			} else {
+				myself.GetComponent<Transform> ().Translate (0.0f, -court * Time.deltaTime, 0.0f);
+			}
+		}
+		if (Input.GetKey (KeyCode.Q)) {
+			skin.GetComponent<Transform> ().localRotation = Quaternion.Euler (0.0f, 0.0f, 90.0f);
+			if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) {
+				myself.GetComponent<Transform> ().Translate (-marche * Time.deltaTime, 0.0f, 0.0f);
+			} else {
+				myself.GetComponent<Transform> ().Translate (-court * Time.deltaTime, 0.0f, 0.0f);
+			}
+		} else if (Input.GetKey (KeyCode.D)) {
+			skin.GetComponent<Transform> ().localRotation = Quaternion.Euler (0.0f, 0.0f, -90.0f);
+			if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) {
+				myself.GetComponent<Transform> ().Translate (marche * Time.deltaTime, 0.0f, 0.0f);
+			} else {
+				myself.GetComponent<Transform> ().Translate (court * Time.deltaTime, 0.0f, 0.0f);
+			}
+		} else {
+			skin.GetComponent<Transform> ().localRotation = Quaternion.Euler (0.0f, 0.0f, rotateSkin);
+		}*/
+	}
+}
