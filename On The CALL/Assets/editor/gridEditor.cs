040bac03501a87d99da3676dfa32d8bf08bdc0a0
loulou123546 <loulou.valleres@live.fr>
2018-10-25 14:47:41 +0200

    Add Unity files and plugin

---
+++ On The CALL/Assets/editor/gridEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+[CustomEditor(typeof(Grid))]
+public class GridEditor : Editor
+{
+	Grid grid;
+	public void OnEnable()
+	{
+		grid = (Grid)target;
+		SceneView.onSceneGUIDelegate += GridUpdate;
+	}
+
+	public void OnDisable()
+	{
+		SceneView.onSceneGUIDelegate -= GridUpdate;
+	}
+	void GridUpdate(SceneView sceneview)
+	{
+		Event e = Event.current;
+		Ray r = Camera.current.ScreenPointToRay(new
+			Vector3(e.mousePosition.x, -e.mousePosition.y +
+				Camera.current.pixelHeight));
+		Vector3 mousePos = r.origin;
+		if(e.isKey && e.character == 'a')
+		{
+			GameObject obj;
+			//Object prefab =
+			//	EditorUtility.GetPrefabParent(Selection.activeObject);
+			obj = (GameObject)EditorUtility.InstantiatePrefab(grid.prefab);
+			Vector3 aligned = new Vector3(
+				Mathf.Floor(mousePos.x/grid.width)*grid.width + grid.width/2.0f,
+				Mathf.Floor(mousePos.y/grid.height)*grid.height +
+				grid.height/2.0f,
+				0.0f);
+			obj.transform.position = aligned;
+		}
+	}
+}
