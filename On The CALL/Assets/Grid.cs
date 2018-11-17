2ecded068b83b3c9526c9c3ce7542bbcb9cea892
loulou123546 <loulou.valleres@live.fr>
2018-11-17 23:25:18 +0100

    remove old road and add 9 street + parc + tree + first concrete

--- On The CALL/Assets/Grid.cs
+++ On The CALL/Assets/Grid.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 [AddComponentMenu("Custom/Editor")]
+
 public class Grid : MonoBehaviour
 {
 	public Object prefab;
+	public bool placeOnNext = false;
 	public float width = 5.0f;
 	public float height = 5.0f;
 	public Color color = Color.yellow;
@@ -14,16 +16,16 @@ public class Grid : MonoBehaviour
 		for (float y = pos.y - 250.0f; y < pos.y + 250.0f; y+= height)
 		{
 			Gizmos.DrawLine(
-				new Vector3(-250.0f, Mathf.Floor(y/height) * height, 0.0f),
-				new Vector3(250.0f, Mathf.Floor(y/height) * height, 0.0f));
+				new Vector3(-250.0f + pos.x, Mathf.Floor(y/height) * height, 0.0f),
+				new Vector3(250.0f + pos.x, Mathf.Floor(y/height) * height, 0.0f));
 		}
 
 		for (float x = pos.x - 250.0f; x < pos.x + 250.0f; x+= width)
 		{
-
+			
 			Gizmos.DrawLine(
-				new Vector3(Mathf.Floor(x/width) * width, -250.0f, 0.0f),
-				new Vector3(Mathf.Floor(x/width) * width, 250.0f, 0.0f));
+				new Vector3(Mathf.Floor(x/width) * width, -250.0f + pos.y, 0.0f),
+				new Vector3(Mathf.Floor(x/width) * width, 250.0f + pos.y, 0.0f));
 		}
 	}
 }
