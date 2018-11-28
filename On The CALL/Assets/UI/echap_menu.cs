977d323a57b307bf5ab209fcc6d06055aa679a41
loulou123546 <loulou.valleres@live.fr>
2018-11-28 18:47:01 +0100

    add some functionality
    implemented only this WE

--- On The CALL/Assets/UI/echap_menu.cs
+++ On The CALL/Assets/UI/echap_menu.cs
@@ -10,6 +10,9 @@ public class echap_menu : MonoBehaviour {
 	public bool OnPause = false;
 	public GameObject Canvas_game;
 	public GameObject Canvas_pause;
+	public GameObject Chat_zone;
+	public GameObject Chat_input;
+	public GameObject Chat_button;
 
 	// Use this for initialization
 	void Start () {
@@ -33,6 +36,14 @@ public class echap_menu : MonoBehaviour {
 			Application.Quit ();
 		}
 	}
+	IEnumerator SendMessage (string text = "no message", string pseudo = "no name"){
+		WWWForm formMsg = new WWWForm ();
+		formMsg.AddField ("user", pseudo);
+		formMsg.AddField ("msg", text);
+		WWW downloadMsg = new WWW ("https://cta.loulou123546.fr/gameAPI/newMSG", formMsg);
+		yield return downloadMsg;
+		Chat_button.GetComponent<Button> ().interactable = true;
+	}
 
 	public void BackMenu (){
 		StartCoroutine (Disconnect ("disconnect", 0));
@@ -52,5 +63,17 @@ public class echap_menu : MonoBehaviour {
 			Canvas_pause.SetActive (true);
 			Canvas_game.SetActive (false);
 		}
+	}
+
+	public void SendMessage (){
+		Chat_button.GetComponent<Button> ().interactable = false;
+		string presend = Chat_input.GetComponent<InputField> ().text;
+		string author = PlayerPrefs.GetString ("user_name", "Inconnu");
+		Chat_input.GetComponent<InputField> ().text = "";
+		StartCoroutine (SendMessage(presend, author));
+	}
+
+	public void DEV_respawn (){
+
 	}
 }
